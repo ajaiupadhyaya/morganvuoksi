5a4bf7a7c9f283fdae310a1d0dbdd8254ad7aa79
Ajai Upadhyaya <137075998+ajaiupadhyaya@users.noreply.github.com>
2025-06-17 16:57:56 -0400

    Add market data loader with technical indicators

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,3 +1,4 @@
 from .fetcher import DataFetcher
+from .market_data import MarketDataLoader, load_market_data
 
-__all__ = ["DataFetcher"]
+__all__ = ["DataFetcher", "MarketDataLoader", "load_market_data"]
